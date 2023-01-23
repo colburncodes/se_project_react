571d0b93b9e91b6966d8661c75f9361a293773d1
colburncodes <colburnsanders@gmail.com>
2023-01-22 19:03:22 -0600

    refactoring item card for preview

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./ItemCard.css";
-
-export function ItemCard({ name, url, alt }) {
+export function ItemCard(data, onCardClick) {
+  console.log(data);
   return (
-    <div className="card">
+    <div className="card" onClick={() => onCardClick}>
       <div className="card__content">
         <div className="card__title-background">
-          <h2 className="card__title">{name}</h2>
+          <h2 className="card__title">{data.name}</h2>
         </div>
-
         {/* <img src="./images/heart.svg" alt="Heart Icon" /> */}
-        <img className="card__image" src={url} alt={alt} />
+        <img className="card__image" src={data.link} alt={data.name} />
       </div>
     </div>
   );
 }
-
