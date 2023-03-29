68e24515d2c86514c2281e9600537e909a61d27b
colburncodes <colburnsanders@gmail.com>
2023-03-29 16:54:37 -0500

    likes only show when user signs in

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -2,28 +2,37 @@ import React, { useContext } from "react";
 import likeicon from "../../images/heart.svg";
 import "./ItemCard.css";
 import { CurrentUserContext } from "../../context/CurrentUserContext";
-export function ItemCard({ card, onCardClick, handleLikeClick }) {
+export function ItemCard({ isLoggedIn, card, onCardClick, handleLikeClick }) {
   const currentUser = useContext(CurrentUserContext);
   const isLiked = card.likes.some((user) => user === currentUser._id);
 
+  // const itemLikeButtonClassName = isLoggedIn
+  //   ? `card__heart-icon ${isLiked ? "card__heart-icon" : "card__like-btn"}`
+  //   : "card__like-btn_hidden";
+
   const itemLikeButtonClassName = `card__heart-icon ${
-    isLiked ? "card__heart-icon" : "card__like-btn" //"card__like-btn"
+    isLiked ? "card__heart-icon" : "card__like-btn"
   }`;
+
   return (
     <div className="card" onClick={() => onCardClick(card)}>
       <div className="card__content">
         <div className="card__title-background">
           <h2 className="card__title">{card.name}</h2>
-          <img
-            className={itemLikeButtonClassName}
-            src={likeicon}
-            alt={card.name}
-            onClick={(e) => {
-              e.stopPropagation();
-              console.log({ isLiked: isLiked });
-              handleLikeClick(card._id, !isLiked);
-            }}
-          />
+          {isLoggedIn ? (
+            <img
+              className={itemLikeButtonClassName}
+              src={likeicon}
+              alt={card.name}
+              onClick={(e) => {
+                e.stopPropagation();
+                console.log({ isLiked: isLiked });
+                handleLikeClick(card._id, !isLiked);
+              }}
+            />
+          ) : (
+            <img className="card__heart-icon" src={likeicon} alt={card.name} />
+          )}
         </div>
         <img className="card__image" src={card.imageUrl} alt={card.name} />
       </div>
