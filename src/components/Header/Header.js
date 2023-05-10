88d80b5933a44213dc2494dd023fb17ee9529d70
colburncodes <colburnsanders@gmail.com>
2023-05-09 20:37:07 -0500

    style: :lipstick: started material ui conversion

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,43 @@ import { ToggleSwitch } from "../ToggleSwitch/ToggleSwitch";
 import { CurrentUserContext } from "../../context/CurrentUserContext";
 import "./Header.css";
 
+import { styled, Stack } from "@mui/material";
+
+import Container from "@mui/material/Container";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+
+const Menu = styled(Box)({
+  borderBottom: "1px solid black",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  paddingTop: 2,
+});
+
+const MenuItem = styled(Button)({
+  fontWeight: 900,
+  fontSize: "12px",
+  marginRight: "2px",
+});
+
+const Logo = styled("div")({
+  fontWeight: 900,
+  color: "black",
+  fontSize: "34px",
+  cursor: "pointer",
+});
+
+const LogoDesc = styled(Box)({
+  fontWeight: 600,
+  color: "black",
+  fontSize: "16px",
+  marginTop: "5px",
+});
+
 export function Header({
   isLoggedIn,
   weatherData,
@@ -16,55 +53,77 @@ export function Header({
   const currentUser = useContext(CurrentUserContext);
 
   if (!weatherData) return null;
+
   return (
-    <header className="header">
-      <div className="header__container">
-        <Link to={"/"}>
-          <img className="header__logo" src={logoPath} alt="What To Wear" />
-        </Link>
-        <p className="header__date-location">
-          {currentDate}, {weatherData.name}
-        </p>
-      </div>
-      <div className="header__info">
-        <ToggleSwitch />
-        {isLoggedIn ? (
-          <div className="header__info-user">
-            <button
-              className="header__add-clothes"
-              type="button"
-              onClick={onAddClick}
-            >
-              + Add clothes
-            </button>
-            <p className="header__username">{currentUser.name}</p>
-            <Link to={"/profile"}>
-              <img
-                className="header__avatar"
-                alt="Avatar"
-                src={currentUser.avatar}
-              />
-            </Link>
-          </div>
-        ) : (
-          <>
-            <button
-              className="header__register"
-              type="button"
-              onClick={onRegisterClick}
-            >
-              Sign Up
-            </button>
-            <button
-              className="header__login"
-              type="button"
-              onClick={onLoginClick}
-            >
-              Log in
-            </button>
-          </>
-        )}
-      </div>
-    </header>
+    <>
+      <Container>
+        <AppBar position="static" color="transparent" elevation={0}>
+          <Menu>
+            <Stack direction={"row"} flex={1}>
+              <Link to={"/"} style={{ textDecoration: "none" }}>
+                <Logo>wtwr</Logo>
+              </Link>
+
+              <Stack direction={"column"} marginTop={1} marginLeft={1}>
+                <LogoDesc>
+                  {currentDate}, {weatherData.name}
+                </LogoDesc>
+              </Stack>
+            </Stack>
+
+            {isLoggedIn ? (
+              <Stack direction={"row"} m={1}>
+                <div className="header__info-user">
+                  <button
+                    className="header__add-clothes"
+                    type="button"
+                    onClick={onAddClick}
+                  >
+                    + Add clothes
+                  </button>
+                  <p className="header__username">{currentUser.name}</p>
+                  <Link to={"/profile"}>
+                    <img
+                      className="header__avatar"
+                      alt="Avatar"
+                      src={currentUser.avatar}
+                    />
+                  </Link>
+                </div>
+              </Stack>
+            ) : (
+              <>
+                <Stack direction={"row"} m={1}>
+                  {/* <ToggleSwitch /> */}
+
+                  <Button
+                    onClick={onLoginClick}
+                    color="inherit"
+                    sx={{
+                      color: "#000",
+                      fontWeight: "bold",
+                      marginRight: "5px",
+                    }}
+                  >
+                    Log in
+                  </Button>
+                  <MenuItem
+                    onClick={onRegisterClick}
+                    color="inherit"
+                    sx={{
+                      color: "#0049fb",
+                      fontWeight: "bold",
+                      border: "1px solid #0049fb",
+                    }}
+                  >
+                    Sign up
+                  </MenuItem>
+                </Stack>
+              </>
+            )}
+          </Menu>
+        </AppBar>
+      </Container>
+    </>
   );
 }
