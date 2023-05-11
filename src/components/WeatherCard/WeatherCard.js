1e0a779d62d1c90f8a04b1093ee9e2dc5e14c505
colburncodes <colburnsanders@gmail.com>
2023-05-10 20:47:14 -0500

    refactor: :lipstick: updated ui

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -4,6 +4,16 @@ import cloudPath from "../../images/Union.svg";
 import sunPath from "../../images/sun.svg";
 import { CurrentTemperatureUnitContext } from "../../context/CurrentTemperatureUnitContext";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material";
+
+const Menu = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  paddingTop: 2,
+  marginTop: 5,
+});
 
 export function WeatherCard({ weatherData }) {
   const { currentTemperatureUnit } = React.useContext(
@@ -13,19 +23,22 @@ export function WeatherCard({ weatherData }) {
 
   return (
     <Container>
-      <div className="weather__container">
-        {currentTemperatureUnit === "F" ? (
-          <p className="weather__temperature">
-            {Math.round(temperature)}°{currentTemperatureUnit}
-          </p>
-        ) : (
-          <p className="weather__temperature">
-            {Math.round(((temperature - 32) * 5) / 9)}°{currentTemperatureUnit}
-          </p>
-        )}
-        <img className="weather__sunny" src={sunPath} alt="sun" />
-        <img className="weather__cloud-union" src={cloudPath} alt="clouds" />
-      </div>
+      <Menu>
+        <div className="weather__container">
+          {currentTemperatureUnit === "F" ? (
+            <p className="weather__temperature">
+              {Math.round(temperature)}°{currentTemperatureUnit}
+            </p>
+          ) : (
+            <p className="weather__temperature">
+              {Math.round(((temperature - 32) * 5) / 9)}°
+              {currentTemperatureUnit}
+            </p>
+          )}
+          <img className="weather__sunny" src={sunPath} alt="sun" />
+          <img className="weather__cloud-union" src={cloudPath} alt="clouds" />
+        </div>
+      </Menu>
     </Container>
   );
 }
